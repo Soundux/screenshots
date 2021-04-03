feb4a66c63d87a2ef3440369b6e788b338701402
D3SOX <d3sox@protonmail.com>
2021-04-03 13:51:27 +0200

    feat: add readme to screenshots folder

--- src/screenshot.js
+++ src/screenshot.js
@@ -1,5 +1,10 @@
-const path = require('path');
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
+import { markdownTable } from 'markdown-table';
+import { dirname, join } from 'path';
+import { writeFile, readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const [width, height] = [1360, 795];
 const output = {
@@ -8,7 +13,7 @@ const output = {
     'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAABRhJREFUWIXll0mIXEUYx39fvdevu2emJzOTzKommURioiBOTIyiiPEgGkPigqDgyVvwoggiHjwGQXJzJ3hwkGDEJS6gQRRFXAKiCAnjlsk2E2OI9KTT4/RS9Xl4r7vfe90ZJYoeLCi6urb/v/7fUvXg/17kAscupOhfAvng6fsLG9f27/CNuwa1GdSBOlSthm1Lo6/xX8/T3xrXWl3NgQOHef72xz4unZfA27vuKty8fuwTT9xEcoP4pjYFFo279v5wzIIqqMXif/3RFJvvfOKLJgkTJ7Bp3dIHQ3AXLqJRSbVTgmpa3Y5q42nl6mvH7Y54nx9Xw2A3oSbasCG9RifQiIO2yKmG4x1BtWPbaPU6QuU1rYCos1nUJcGJyCR+06DaoXYgoYpzGhAzfUKBpk2JnzKyqcnC0quwfi/VXw6Qq51EYoC2exwzcj1ybhpOfQ71chO0qV4HpeIEUHWhQ4V/ohMLXLKFemEt1eJRVB2Zyx5AeoYhKIT71ebwyrPM/3YE0ztBsPJezMn9cPjVaI+4SsmSImBBvRZzFF2+HR25EaPQNbCO5HwNxcwug+wyugauBMDNn0LH78NJHvn+xZhS7QokokASoeZQ8WD0JkzQi8n2toOfp5iuYcTPIcu3Uq4HLUfuoEKCQDLGLQxchWR62gAWA4/PkUw3ZvRGypWYX6WWmuSiVMIZ3HhB4PESXHwTzikLVReSWNwEEbBzKAb6Lk+C2xq/T+9n4finCSLq6ixMv0/12IeRI7fG/KEJjOfjVKnVGqZolZQTRmEH0LMS8fOJU5cO7eHs1JuA0l8r07XqNgB+n9pD+dArGBw9lSLBpXe3NvVy+P1r0DPfoTjq1sUh23xAQwUsdC9HE5kOKsUjOOdQZ6kVp5v99bmjoQPjqBd/Jl38vkvxBAyKWdwJXSShRXNDbRv1rL4NYzy8IE/X+C3N/vyqLRg/wPh5gvGtbetM9xgiDiOKSd2/CROEp49mBIVWtzpEDLnRDYxun0SMh3jZ5nhmeD39W19DAfFz0RqLiAeABL14oql80EEBbV6vNjmvMkflyAfYczOYTFcCPFyn4OcQP4fOn8ROvwFzP6HOhhNcrQmuJH0gpYADjRT46WW0dzXSsxyCAsHStVSO7Wd+7hh+4WK83hWYXH+4rHoWPTuNlqYxQQHvos2QGwSto8UfYeoFWvfKIlEgEQFB0YXT6JcPU7noDjIrbsF0jZIZ2kD5h31UT3yGiGLEISgGh4ji+Xn8DY+jhXGk8hv8PAmH94JdIPnGOA+BuPSCInaezJFXKB6aRLID4OpotYRII55DYGl4d72E/WZn2K4VEWJPNBrX/CIEwsmJDjyBvrxSLJ8O02sT0EVhRagALnS0ahGJlBHVVvZr5JhFUrFaWy9p5IQavfXUWQyOJXnFNw5Ri8FiGmGFRcThoQiuaZaz8+6oph+qfxIF+uNM+T1V1eTrNqyeOArZkETj9BIR8XDNOJfI2Q6esLvPlOpfJUwQz7SdCGx+/OBbU8fPPeectW03Y3Ty7kDxjEOwIXADPGo3AK+4xLvsnl2z9/06V/skCZ4kkP4uEKDw0LbhiRvW9UwYEb/93QfOKXWrzROHfkHL1igZ38xse3Jm75pRf+SdRwd3j/WZW1FHqaLvje34ZVsbk1QxQAYI/kZtOLgZXmJGDj419HrppWGdfXb4XdJXwL9QpDvL4Lc7ByZnnxn8TwgAiAgD+x7pu5N//rvzr5MglXv+ANNZT+AVaZNqAAAAAElFTkSuQmCC',
 };
 
-const start = async () => {
+try {
   const browser = await puppeteer.launch({
     defaultViewport: { height, width },
   });
@@ -107,7 +112,7 @@ const start = async () => {
   });
 
   // load ui
-  await page.goto(`file:${path.join(__dirname, '../dist/index.html')}`);
+  await page.goto(`file:${join(__dirname, '../dist/index.html')}`);
 
   // convenience functions
   const ss = async (name, element) => {
@@ -222,10 +227,28 @@ const start = async () => {
   const bottomSheet = await page.waitForSelector('.v-bottom-sheet');
   await ssThemed('media-controls', bottomSheet);
 
+  // generate readme
+  console.debug('Generating screenshot overview readme...');
+  const table = [['Name', 'Preview (direct link)']];
+
+  const screenshotsDir = join(__dirname, '../screenshots');
+  const dir = await readdir(screenshotsDir);
+  dir.forEach(file => {
+    if (file.endsWith('.png')) {
+      const link = `https://raw.githubusercontent.com/Soundux/screenshots/screenshots/${file}`;
+      table.push([file.substring(0, file.length - 4), `[![${file}](${link})](${link})`]);
+    }
+  });
+
+  const screenshotsTable = markdownTable(table);
+  const readmeFile = `# Screenshots overview\n` + screenshotsTable;
+  await writeFile(join(screenshotsDir, 'README.md'), readmeFile);
+
+  // close browser
   await page.close();
   await browser.close();
-};
+} catch (error) {
+  console.error(error);
+}
 
-start()
-  .then(() => console.log('Successfully generated screenshots'))
-  .catch(error => console.error(error));
+console.log('Successfully generated screenshots');
